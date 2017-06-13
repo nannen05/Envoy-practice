83f34b628ea22f7baa42dc645fec06456c8d7593
nannen05 <nannen05@gmail.com>
2017-06-13 12:23:03 -0700

    fixed slider colors

--- components/slider.js
+++ components/slider.js
@@ -5,32 +5,28 @@ import React from 'react';
 const SliderData = [
     {
         title: "The Iphone Unchained.",
+        titleColor: "red",
         description: "Tarantino’s outrageous film Djano Unchained needed an equally in your face mobile experience. Bloody good awesomeness ensued.",
         imageUrl: "images/django.png",
         link: "#",
         position: "top-left"
     },
     {
-        title: "The Iphone Unchained",
-        description: "Boost Mobile recognized the need to elevate and refresh its brand to speak to a broader audience.",
-        imageUrl: "https://d3ciwvs59ifrt8.cloudfront.net/0f85269b-d32e-41c5-8859-fa3f391e0897/24a15f21-c96a-42d4-9128-6509480fbb25_h.png",
+        title: "SAY HELLO TO THE future of vizio",
+        titleColor: "red",
+        description: "When Vizio needed to pull the wrap off its new family of PCs they called on Envoy to conceptualize, direct and produce a series of short product films.",
+        imageUrl: "images/vizio.png",
         link: "#",
-        position: "bottom"
+        position: "top-right"
     },
     {
-        title: "The Iphone Unchained",
-        description: "Boost Mobile recognized the need to elevate and refresh its brand to speak to a broader audience.",
-        imageUrl: "https://d3ciwvs59ifrt8.cloudfront.net/0f85269b-d32e-41c5-8859-fa3f391e0897/24a15f21-c96a-42d4-9128-6509480fbb25_h.png",
+        title: "WORLD MEET ULTRA. ULTRA MEET WORLD.",
+        titleColor: "blue",
+        description: "Asked to launch T-Mobile’s new internationally-minded brand, Ultra Mobile, we created a campaign that’s creativity knows no borders",
+        imageUrl: "images/data.png",
         link: "#",
-        position: "bottom-right"
+        position: "top-left"
     },
-    {
-        title: "The Iphone Unchained",
-        description: "Boost Mobile recognized the need to elevate and refresh its brand to speak to a broader audience.",
-        imageUrl: "https://d3ciwvs59ifrt8.cloudfront.net/0f85269b-d32e-41c5-8859-fa3f391e0897/24a15f21-c96a-42d4-9128-6509480fbb25_h.png",
-        link: "#",
-        position: "bottom-right"
-    }
 
 ]
 
@@ -41,6 +37,8 @@ class SliderItem extends React.Component {
     render () {
 
       let sliderPosition = "slider-position slider-position__" + this.props.data.position;
+      let titleColor = "header-" + this.props.data.titleColor;
+      let linkColor = "link-" + this.props.data.titleColor;
 
       return <div className="component-slider__item">
                   <div className="image-wrapper">
@@ -50,10 +48,10 @@ class SliderItem extends React.Component {
                       <div className="container">
                         <div className={sliderPosition}>
                           <div className="header-wrapper">
-                              <h2>{this.props.data.title}</h2>
+                              <h2 className={titleColor}>{this.props.data.title}</h2>
                           </div>
                           <div className="content-wrapper">
-                            <p>{this.props.data.description} <a href={this.props.data.link}>View Project</a></p>
+                            <p>{this.props.data.description} <a className={linkColor} href={this.props.data.link}>View Project</a></p>
                           </div>
                         </div>
                       </div>
